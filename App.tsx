a5e0b3db6fc89f9a0d18b830a0f5b671f74bd35b
MinhThu <73973384+MinhThu100200@users.noreply.github.com>
2023-11-07 20:12:35 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -104,9 +104,6 @@ function App(): JSX.Element {
         style={backgroundStyle}>
         <TouchableOpacity
           onPress={() => Crashes.generateTestCrash()}
-          // onPress={() => {
-          //   throw new Error('error');
-          // }}
           style={{
             backgroundColor: 'gray',
             paddingVertical: 5,
